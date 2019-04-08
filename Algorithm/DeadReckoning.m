c0a57b0e69630713ab1211d90a8ec37e8df1a994
744475118@qq.com <744475118@qq.com>
2019-04-08 19:54:31 +0800

    update

--- Algorithm/DeadReckoning.m
+++ Algorithm/DeadReckoning.m
@@ -1,16 +1,15 @@
-function [estimatorDR] = DeadReckoning(robot, sampletime, iterator)
+function [estimatorDR] = DeadReckoning(mut_1, ut, sampletime)
 
-odometry = robot.odometry(:, iterator);
-vt=odometry(1);
-omegat=odometry(2);
-period = robot.estimatorDR(:, iterator - 1);
+vt = ut(1);
+omegat = ut(2);
+period = mut_1;
 theta = wrapToPi(period(3));
 
 if omegat~=0
-    estimatorDR = period + [-vt/omegat*sin(theta)+vt/omegat*sin(theta+omegat*sampletime);vt/omegat*cos(theta)-vt/omegat*cos(theta+omegat*sampletime);omegat*sampletime];
+    estimatorDR = period + [-vt/omegat*sin(theta)+vt/omegat*sin(theta+omegat*sampletime);vt/omegat*cos(theta) - vt/omegat*cos(theta+omegat*sampletime);omegat*sampletime];
 else
     estimatorDR = period + [vt*sampletime*cos(theta);vt*sampletime*sin(theta);0];
 end
-estimatorDR(3)=wrapToPi(estimatorDR(3));
+    estimatorDR(3)=wrapToPi(estimatorDR(3));
 end
 
--- Localization/EKFpara_Localization.m
+++ Localization/EKFpara_Localization.m
@@ -11,39 +11,29 @@ global delt iterator alpha SigmaQ;
         time = 0;
         delt = 0.1;
         iterator = ceil((end_time - start_time)/delt);
+        
+        robot = linearAngular(1, [0, 0, 0], [0, 0], eye(3));
 
-        alpha = [0.01, 0.001, 0.001, 0.01];
-        SigmaQ = diag([0.1 ToRadian(20)]).^2; % Sensor noise
+        alpha = robot.controlParameter;
+        SigmaQ = robot.sensorNoise; % Sensor noise
 
-        mu_init = [0; 0; 0];
-        Sigma_init = eye(3);
+        mu_init = robot.groundTruth;
+        Sigma_init = robot.sigmaEKF;
         
         mut_1 = mu_init;
         Sigmat_1 = Sigma_init;
 
         m = [1, 2, 1; 2, 2, 2; 3, 3, 3];
-
+        
         for i = 1 : iterator          
             time = time + i * delt; 
             ut = doControl(time);      
             ground = CalGround(mut_1, ut, delt);
-            zt = Observation(ground, m);
-            EKF_localization(mut_1, Sigmat_1, ut, zt, m, delt);
-            
-            
-            
-            
-            
-            
+            zt = CalObservation(ground, m);
+            [robot.estimatorEKF(:, i + 1), robot.sigmaEKF{i}] = EKF_localization(mut_1, Sigmat_1, ut, zt, m, delt);
+            robot.estimatorDR(:, i + 1) = DeadReckoning(mut_1, ut, delt);
         end
         
-        
-
-        
-
-
-
-
 
     elseif nargin == 5 && nargout == 2
 
@@ -116,7 +106,7 @@ if ~isempty(zt)
         St = Ht*Sigma_t*(Ht)' + SigmaQ;
         %St=Ht*Sigma_t*(Ht)'+Qt+Hm*Sigma_m(:, 1+(N-1)*3 :3+(N-1)*3)*(Hm)';
         Kt = Sigma_t*(Ht)'*inv(St);
-        mu_t = mu_t+Kt*(z_t(n,1:2)' - zt_Lambda);
+        mu_t = mu_t + Kt*(z_t(n,1:2)' - zt_Lambda);
         Sigma_t = (I - Kt*Ht)*Sigma_t;
     end
 end
@@ -135,12 +125,12 @@ T = 10; % [sec]
 V = 1.0; % [m/s]
 omega = 5; % [deg/s]
  
-u =[ V*(1-exp(-time/T)) toRadian(omega)*(1-exp(-time/T))]';
+u =[ V*(1-exp(-time/T)) ToRadian(omega)*(1-exp(-time/T))]';
 end
 
-function zt = Observation(ground, object)
+function zt = CalObservation(ground, object)
      q = (ground(1) - object(:, 1)).^2 + (ground(2) - object(:, 1)).^2;
-     zt = [sqrt(q), atan2(ground(2) - object(:, 2), ground(1) - object(:, 1)) - ground(:, 3), object(:, 3)];
+     zt = [sqrt(q), atan2(ground(2) - object(:, 2), ground(1) - object(:, 1)) - ground(3), object(:, 3)];
 end
 
 function groundTruth = CalGround(mut_pre, ut, sampletime)
