7de5fa213cec649cd50f397c633102f883d9a44e
744475118@qq.com <744475118@qq.com>
2019-04-08 20:30:13 +0800

    update

--- Localization/EKFpara_Localization.m
+++ Localization/EKFpara_Localization.m
@@ -1,7 +1,7 @@
+
 function [varargout] = EKFpara_Localization(varargin)
 
-global delt iterator alpha SigmaQ;
-
+    global delt iterator alpha_noise SigmaQ;
     if nargin == 0
 
         clear; close all; clc;
@@ -14,7 +14,7 @@ global delt iterator alpha SigmaQ;
         
         robot = linearAngular(1, [0, 0, 0], [0, 0], eye(3));
 
-        alpha = robot.controlParameter;
+        alpha_noise = robot.controlParameter;
         SigmaQ = robot.sensorNoise; % Sensor noise
 
         mu_init = robot.groundTruth;
@@ -46,6 +46,8 @@ end
 % function of ekf localization
 function [mut, sigmat] = EKF_localization(mut_1, Sigmat_1, ut, zt, m, sampletime)
 
+global alpha_noise;
+
 theta = mut_1(3);
 vt = ut(1);
 omegat = ut(2);
@@ -64,8 +66,8 @@ if omegat ~= 0
     Vt = [(-sin(theta) + sin(theta+omegat*deltat))/omegat,vt*(sin(theta) - sin(theta+omegat*deltat))/omegat^2 + vt*cos(theta+omegat*deltat)*deltat/omegat;
         (cos(theta) - cos(theta + omegat*deltat))/omegat,-vt*(cos(theta) - cos(theta+omegat*deltat))/omegat^2 + vt*sin(theta+omegat*deltat)*deltat/omegat;
         0,deltat];
-    Mt = [alpha(1)*vt^2 + alpha(2)*omegat^2, 0;
-        0, alpha(3)*vt^2 + alpha(4)*omegat^2];
+    Mt = [alpha_noise(1)*vt^2 + alpha_noise(2)*omegat^2, 0;
+        0, alpha_noise(3)*vt^2 + alpha_noise(4)*omegat^2];
 
     mu_t = mut_1 + [(-vt/omegat*sin(theta) + vt/omegat*sin(theta + omegat*deltat));
                      vt/omegat*cos(theta) - vt/omegat*cos(theta + omegat*deltat);
@@ -80,8 +82,8 @@ elseif omegat == 0
     Vt = [deltat*cos(theta),0;
           deltat*sin(theta),0;
           0,0];
-    Mt = [alpha(1)*vt^2 + alpha(2)*omegat^2, 0;
-          0, alpha(3)*vt^2 + alpha(4)*omegat^2];
+    Mt = [alpha_noise(1)*vt^2 + alpha_noise(2)*omegat^2, 0;
+          0, alpha_noise(3)*vt^2 + alpha_noise(4)*omegat^2];
     mu_t = mut_1 + [vt*deltat*cos(theta);
                     vt*deltat*sin(theta);0];
     Sigma_t = Gt*Sigmat_1*Gt' + Vt*Mt*Vt';    
