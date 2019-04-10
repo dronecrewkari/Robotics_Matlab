ea82fe39288a8ca58c7abfbd98d93c7610e6f9c2
744475118@qq.com <744475118@qq.com>
2019-04-10 14:23:38 +0800

    Update RMSE.m

--- ResultProcess/evaluation/RMSE.m
+++ ResultProcess/evaluation/RMSE.m
@@ -1,9 +1,9 @@
 function rmse = RMSE(actualX, actualY, estimateX, estimateY, n)
-b=0;
-for a=1:n
+b = 0;
+for a = 1:n
     actual = sqrt(actualX(a)^2 + actualY(a)^2);
     estimate = sqrt(estimateX(a)^2 + estimateY(a)^2);
-    b=b+(actual-estimate)^2;
+    b=b + (actual-estimate)^2;
 end
 
 rmse = sqrt(b/n);
