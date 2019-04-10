2f28fcbdbb83c8e9c27b5a024f4033361b3c54d5
744475118@qq.com <744475118@qq.com>
2019-04-10 20:54:57 +0800

    Update RMSE.m

--- ResultProcess/evaluation/RMSE.m
+++ ResultProcess/evaluation/RMSE.m
@@ -3,7 +3,7 @@ b = 0;
 for a = 1:n
     actual = sqrt(actualX(a)^2 + actualY(a)^2);
     estimate = sqrt(estimateX(a)^2 + estimateY(a)^2);
-    b=b + (actual-estimate)^2;
+    b = b + (actual-estimate)^2;
 end
 
 rmse = sqrt(b/n);
