84930b4fc14e4f50dd7e7d14ae3b5dffa199be01
744475118@qq.com <744475118@qq.com>
2019-04-10 11:14:30 +0800

    Update drawGraphTemplate.m

--- ResultProcess/graph/drawGraphTemplate.m
+++ ResultProcess/graph/drawGraphTemplate.m
@@ -1,18 +1,19 @@
-function [] = drawGraphTemplate(groundTruth, estimatorTarget, estimatorCompare)
+function [] = drawGraphTemplate(groundTruth, estimatorTarget, estimatorCompare, nameGround, nameTarget, nameCompare)
 
 figEKF = figure(1);
 set(figEKF, 'name', 'localization by EKF')
 
-plot(groundTruth(1, :), groundTruth(2, :), '-g', 'linewidth', 2); hold on;
-plot(estimatorTarget(1, :), estimatorTarget(2, :), 'xr', 'linewidth', 2); hold on;
-plot(estimatorCompare(1, :), estimatorCompare(2, :), '--b', 'linewidth', 2); hold on;
+n = numel(groundTruth, 2);
+t = 1:3:n;
+plot(groundTruth(1, t), groundTruth(2, t), '-g', 'linewidth', 2); hold on;
+plot(estimatorTarget(1, t), estimatorTarget(2, t), 'xr', 'linewidth', 2); hold on;
+plot(estimatorCompare(1, t), estimatorCompare(2, t), '--b', 'linewidth', 2); hold on;
 
-leg = legend('Ground Truth','Target Estimator','Compare Estimator', 'Error Ellipse');
+leg = legend(nameGround, nameTarget, nameCompare, 'Error Ellipse');
 % set(gca,'FontSize',15,'FontName','Times New Roman');
 set(leg,'FontSize',15,'location','NorthWest','FontName','Times New Roman');
 xlabel('x position (unit:m)', 'FontSize',20,'FontName','Times New Roman');
 ylabel('y position (unit:m)', 'FontSize',20,'FontName','Times New Roman');
-zoom on;
 end
 
 
