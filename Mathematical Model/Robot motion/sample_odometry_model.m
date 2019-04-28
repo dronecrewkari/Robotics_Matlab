a9dfa8b810f48926ff351606353433d40283ff88
744475118@qq.com <744475118@qq.com>
2019-04-28 23:01:00 +0800

    Update sample_odometry_model.m

--- Mathematical Model/Robot motion/sample_odometry_model.m
+++ Mathematical Model/Robot motion/sample_odometry_model.m
@@ -1,4 +1,4 @@
-function [xt]=sample_odometry_model(ut,xt_1)
+function [xt] = sample_odometry_model(ut, xt_1)
 %%
 % time t-1 pose
 x=xt_1.x;
@@ -21,12 +21,12 @@ delta_rot1=atan2(y_n-y_,x_n-x_)-theta_;
 delta_trans=sqrt((x_-x_n)^2+(y_-y_n)^2);
 delta_rot2=theta_n-theta_-delta_rot1;
 
-delta_rot1_Lambda = delta_rot1-sample_distribution(alpha_1*(delta_rot1^2)+alpha_2*(delta_trans^2),distribution);
-delta_trans_Lambda = delta_trans-sample_distribution(alpha_3*(delta_trans^2)+alpha_4*(delta_rot1^2)+alpha_4*(delta_rot2^2),distribution);
+delta_rot1_Lambda = delta_rot1 - sample_distribution(alpha_1*(delta_rot1^2)+alpha_2*(delta_trans^2),distribution);
+delta_trans_Lambda = delta_trans - sample_distribution(alpha_3*(delta_trans^2)+alpha_4*(delta_rot1^2)+alpha_4*(delta_rot2^2),distribution);
 delta_rot2_Lambda=delta_rot2-sample_distribution(alpha_1*(delta_rot2^2)+alpha_2*(delta_trans^2),distribution);
 
-xn=x+delta_trans_Lambda*cos(theta+delta_rot1_Lambda);
-yn=y+delta_trans_Lambda*sin(theta+delta_rot1_Lambda);
+xn=x + delta_trans_Lambda*cos(theta+delta_rot1_Lambda);
+yn=y + delta_trans_Lambda*sin(theta+delta_rot1_Lambda);
 thetan=theta+delta_rot1_Lambda+delta_rot2_Lambda;
 
 xt.x=xn;
