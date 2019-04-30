1ee268b270c9f706c6997bfe10196b5ff50299c9
hanruihua <744475118@qq.com>
2019-04-30 15:42:10 +0800

    Update MCL_demo.m

--- Demonstration/localization/MCL_demo.m
+++ Demonstration/localization/MCL_demo.m
@@ -14,7 +14,7 @@ global delt iterator;
         interval = 5; %sample interval
         
         % setting of the parameter of the robot
-        robot = linearAngular(1, [0, 0, 0], [0; 0], eye(3), [0.3, 0.1, 0.1, 0.3], [0.03, 0.03]); %robot model
+        robot = linearAngular(1, [0, 0, 0], [0; 0], eye(3), [0.3, 0.1, 0.1, 0.3, 0, 0], [0.03, 0.03]); %robot model
         alpha_noise = robot.controlParameter; %control noise parameter
         SigmaQ_eigenvalue = robot.sensorNoise; % Sensor noise
             
@@ -37,6 +37,38 @@ global delt iterator;
         alpha_noise = robot.controlParameter; %control noise parameter
         SigmaQ_eigenvalue = robot.sensorNoise; % Sensor noise  
     end
+    
+    for i = 1 : iterator    
+            plot(m(1, :), m(2, :), 'kh', 'MarkerSize', 7);
+            time = time + i * delt; 
+            ut = calOdom(1, 5, time); 
+            
+            robot.odometry(:, i + 1) = ut;
+            ground = calGround(ground_1, ut, alpha_noise, delt);
+            zt = calObservation(ground, m, SigmaQ_eigenvalue);
+            
+            robot.estimatorDR(:, i + 1) = DeadReckoning(DR_1, ut, delt);
+            robot.groundTruth(:, i + 1) = ground;
+            [robot.estimatorEKF(:, i + 1), robot.sigmaEKF{i + 1}] = EKF_localization(mut_1, Sigmat_1, ut, zt, m, alpha_noise, SigmaQ_eigenvalue, delt);
+                      
+            DR_1 = robot.estimatorDR(:, i + 1);
+            ground_1 = ground;
+            mut_1 = robot.estimatorEKF(:, i + 1);
+            Sigmat_1 = robot.sigmaEKF{i + 1};
+            
+            figure(1);
+            set(gcf,'outerposition',get(0,'screensize'));
+            t = 1 : interval : i+1;
+            if rem(i, interval)==0
+               plot(robot.groundTruth(1, t), robot.groundTruth(2, t), '-g', 'linewidth', 2); hold on;
+               plot(robot.estimatorEKF(1, t), robot.estimatorEKF(2, t), 'xr', 'linewidth', 2); hold on;
+               plot(robot.estimatorDR(1, t), robot.estimatorDR(2, t), '--b', 'linewidth', 2); hold on;
+               axis equal;
+               ShowErrorEllipse(robot.estimatorEKF(:, i + 1), robot.sigmaEKF{i + 1}, 1);
+               drawnow;    
+            end
+        end
+    
     
     
     
